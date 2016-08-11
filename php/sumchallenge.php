c95b1c9e46f43186b29c55dcaab4fe998770a0e3
John M Collins <jmc@toad.me.uk>
2016-08-11 22:03:49 +0100

    Format fix of sum.

--- php/sumchallenge.php
+++ php/sumchallenge.php
@@ -24,7 +24,10 @@ $r1 = rand(1,10);
 $r2 = rand(1,10);
 print <<<EOT
 <tr>
-	<td>(Anti-spam) Please answer this sum as a <b>word</b> $r1 + $r2 =</td>
+	<td colspan="2">(Anti-spam) Please answer this sum as a <b>word</b></td>
+</tr>
+<tr>
+   <td>$r1 + $r2 =</td>
 	<td><input type="hidden" name="r1" value="$r1" /><input type="hidden" name="r2" value="$r2" /><input type="text" name="asp" size="20" /></td>
 </tr>
 
