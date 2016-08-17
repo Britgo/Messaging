56c643d118af791ab44fbbff7405eb9366acb87b
John M Collins <jmc@toad.me.uk>
2016-08-17 14:26:04 +0100

    Try again.

--- admin/savealias.php
+++ admin/savealias.php
@@ -152,6 +152,6 @@ EOT;
 
 fclose($outfile);
 
-$docloc = 'https://message.britgo.org/cgi-bin/copyfile' . '?from=' . urlencode($temp_outfile) . '&to=' . urlencode($Alias_dest) . '&next=/admin/index.php';
+$docloc = 'https://message.britgo.org/cgi-bin/copyfile' . '?from=' . urlencode($temp_outfile) . '&to=' . urlencode($Alias_dest) . '&next=' . urlencode('/admin/index.php');
 header("Location: $docloc");
 ?>
