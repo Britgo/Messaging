968e4e7692b496cbf42f3ad881d45b1b12c015ef
John M Collins <jmc@toad.me.uk>
2016-08-24 09:41:18 +0100

    Get format right.

--- index.php
+++ index.php
@@ -176,7 +176,7 @@ EOT;
 </tr>
 <?php include 'php/sumchallenge.php' ?>
 <tr><td align="center"><input type="submit" name="sub" value="Submit"></td>
-<td><a href="admin/index.php" class="admin">Admin</a></td>
+<td align="right"><a href="admin/index.php" class="admin">Admin</a></td>
 </tr>
 </table>
 </form>
